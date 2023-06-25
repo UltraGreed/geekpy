94055f96f074cc8621216f7a06012805d7a341d0
a-pavin <pavin@bk.ru>
2023-06-25 18:05:00 +1000

    Переделал интегрирование с учетом измеренного времени между тиками

--- robot/odometry.py
+++ robot/odometry.py
@@ -7,7 +7,7 @@ from message import X, Y, YAW
 
 TIMER   = 0.05        # Integration and publication timer period.
 MX, MY  = 1.50, 1.00  # Mass in lateral and longitudinal directions.
-TIMEOUT = 0.5         # Force timeout.
+TIMEOUT = 1.0         # Force timeout.
 
 setproctitle.setproctitle(sys.argv[0])         # Set filename.py title for process.
 net    = network.Net(timer=TIMER)              # Network communication with timer event.
@@ -18,20 +18,24 @@ speed               = [0.0, 0.0]  # Last known Motion.speed data.
 speed_time          = 0.0         # Time of Motion.speed refreshing.
 vel_west, vel_north = 0.0, 0.0    # Calculated robot velocity.
 yaw                 = 0.0         # Last known robot yaw position.
+integrator_time     = time.time() # Last integration time.
 
 while net.receive():  # Wait for messages and timer ticks.
 
-    if net.id() == 'Timer':                                                # If timesr has come:
-        if (time.time() - speed_time) > TIMEOUT:                           # If motion speed old
-            speed = [0.0, 0.0]                                             # then stop.
-        acc_x          = (speed[X] - sensor.vel[X]) / MX                   # Calculate new acceleration
-        acc_y          = (speed[Y] - sensor.vel[Y]) / MY                   # in X and Y axis.
-        vel_west      += TIMER * ( acc_x * cosd(yaw) + acc_y * sind(yaw))  # Calculate new
-        vel_north     += TIMER * (-acc_x * sind(yaw) + acc_y * cosd(yaw))  # velocity.
-        sensor.pos[X] += TIMER * vel_west                                  # Update position in
-        sensor.pos[Y] += TIMER * vel_north                                 # outgoing message.
-        sensor.vel[X]  = vel_west * cosd(yaw) - vel_north * sind(yaw)      # Update velocity in
-        sensor.vel[Y]  = vel_west * sind(yaw) + vel_north * cosd(yaw)      # outgoing message.
+    if net.id() == 'Timer':                                             # If timesr has come:
+        now = time.time()                                               # Measuring
+        dt = now - integrator_time                                      # real
+        integrator_time = now                                           # timeout.
+        if (time.time() - speed_time) > TIMEOUT:                        # If motion speed old
+            speed = [0.0, 0.0]                                          # then stop.
+        acc_x          = (speed[X] - sensor.vel[X]) / MX                # Calculate new acceleration
+        acc_y          = (speed[Y] - sensor.vel[Y]) / MY                # in X and Y axis.
+        vel_west      += dt * ( acc_x * cosd(yaw) + acc_y * sind(yaw))  # Calculate new
+        vel_north     += dt * (-acc_x * sind(yaw) + acc_y * cosd(yaw))  # velocity.
+        sensor.pos[X] += dt * vel_west                                  # Update position in
+        sensor.pos[Y] += dt * vel_north                                 # outgoing message.
+        sensor.vel[X]  = vel_west * cosd(yaw) - vel_north * sind(yaw)   # Update velocity in
+        sensor.vel[Y]  = vel_west * sind(yaw) + vel_north * cosd(yaw)   # outgoing message.
         net.send(sensor)
 
     elif net.id() == 'Motion':        # If Motion message has come
--- simulation/imu.py
+++ simulation/imu.py
@@ -4,29 +4,33 @@ sys.path.append('../base')
 import network, message
 from message import YAW
 
-TIMEOUT = 0.50  # Force timeout.
+TIMEOUT = 1.00  # Force timeout.
 PERIOD  = 0.05  # Integration period and publication timer.
-MOMENT  = 0.02  # Momement of inertion in yaw axis.
+MOMENT  = 0.10  # Momement of inertion in yaw axis.
 
 setproctitle.setproctitle(sys.argv[0])  # Set filename.py title for process.
 net    = network.Net(timer=PERIOD)      # Network communication.
 sensor = message.Sensor()               # Sensor class for simulated message with yaw.
 
-yaw_time        = 0.0  # Timestamp of incoming last control values 'Speed'.
-yaw_speed       = 0.0  # Current yaw speed.
-sensor.pos[YAW] = 0.0  # Redefine initial yaw position (None by default).
-sensor.vel[YAW] = 0.0  # Redefine initial yaw velocity (None by default).
-sensor.acc[YAW] = 0.0  # Redefine initial yaw accelerarion (None by default).
+yaw_time        = 0.0         # Timestamp of incoming last control values 'Speed'.
+yaw_speed       = 0.0         # Current yaw speed.
+sensor.pos[YAW] = 0.0         # Redefine initial yaw position (None by default).
+sensor.vel[YAW] = 0.0         # Redefine initial yaw velocity (None by default).
+sensor.acc[YAW] = 0.0         # Redefine initial yaw accelerarion (None by default).
+integrator_time = time.time() # Last integration time.
 
 while net.receive():  # Wait for messages and ticks.
 
-    if net.id() == 'Timer':                                                 # On timer:
-        if time.time() - yaw_time > TIMEOUT:                                # If yaw data too old
-            yaw_speed = 0.0                                                 # then make speed zero.
-        sensor.acc[YAW]  = PERIOD * (yaw_speed - sensor.vel[YAW]) / MOMENT  # Calculate acceleration,
-        sensor.vel[YAW] += PERIOD * sensor.acc[YAW]                         # velocity and
-        sensor.pos[YAW] += PERIOD * sensor.vel[YAW]                         # position.
-        net.send(sensor)                                                    # Send Sensor message.
+    if net.id() == 'Timer':                                             # On timer:
+        now = time.time()                                               # Measuring
+        dt = now - integrator_time                                      # real
+        integrator_time = now                                           # timeout.
+        if time.time() - yaw_time > TIMEOUT:                            # If yaw data too old
+            yaw_speed = 0.0                                             # then make speed zero.
+        sensor.acc[YAW]  = dt * (yaw_speed - sensor.vel[YAW]) / MOMENT  # Calculate acceleration,
+        sensor.vel[YAW] += dt * sensor.acc[YAW]                         # velocity and
+        sensor.pos[YAW] += dt * sensor.vel[YAW]                         # position.
+        net.send(sensor)                                                # Send Sensor message.
 
     elif net.id() == 'Motion':            # If Motion message has come then
         yaw_speed = net.msg().speed[YAW]  # save yaw speed
