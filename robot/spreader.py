591aa8d8cedf1aa4b3b5b4acefc7767c211dacd9
helf4ch <kirill24579@gmail.com>
2023-07-10 17:52:58 +1000

    тестовый спредер

--- robot/spreader.py
+++ robot/spreader.py
@@ -7,12 +7,12 @@ from base import network, message
 
 # Linear (1) & quadric (2) thrusters' spread by axis
 #######     X       Y       DEPTH   YAW     PITCH   ROLL
-SPREAD1 = [[000.00, 000.00, 000.00, 000.00, 000.00, 000.00],  # Thruster 0: bow_left
-           [000.00, 000.00, 000.00, 000.00, 000.00, 000.00],  # Thruster 1: bow_right
-           [000.00, 000.00, 000.00, 000.00, 000.00, 000.00],  # Thruster 2: middle_left
-           [000.00, 000.00, 000.00, 000.00, 000.00, 000.00],  # Thruster 3: middle_right
-           [000.00, 000.00, 000.00, 000.00, 000.00, 000.00],  # Thruster 4: stern_left
-           [000.00, 000.00, 000.00, 000.00, 000.00, 000.00]]  # Thruster 5: stern_right
+SPREAD1 = [[  1.00, 000.00, 000.00, 000.00, 1.00, 000.00],  # Thruster 0: bow_left
+           [ -1.00, 000.00, 000.00, 000.00, 1.00, 000.00],  # Thruster 1: bow_right
+           [ 000.00, 000.00, 1.00, 000.00, 000.00, -1.00],  # Thruster 2: middle_left
+           [ 000.00, 000.00, 1.00, 000.00, 000.00,  1.00],  # Thruster 3: middle_right
+           [ 000.00, 1.00, 000.00,  1.00, 000.00, 000.00],  # Thruster 4: stern_left
+           [ 000.00, 1.00, 000.00, -1.00, 000.00, 000.00]]  # Thruster 5: stern_right
 #######     X       Y       DEPTH   YAW     PITCH   ROLL
 SPREAD2 = [[000.00, 000.00, 000.00, 000.00, 000.00, 000.00],  # Thruster 0: bow_left
            [000.00, 000.00, 000.00, 000.00, 000.00, 000.00],  # Thruster 1: bow_right
@@ -30,7 +30,12 @@ while net.receive():  # Wait for incoming messages.
         speed  = net.msg().speed             # - read Motion.speed vector,
         speed1 = np.array(speed)             # - convert ot numpy array,
         speed2 = speed1 * np.abs(speed1)     # - make signed speed^2 of each value,
-        add    = np.matmul(SPREAD1, speed1)  # - make matrix multiplication of linear part,
-               + np.matmul(SPREAD2, speed2)  # - make matrix multiplication of quadric part,
-        control.power = add.to_list()        # - save sum of parts to message,
+
+        #                   make matrix multiplication of
+        #              linear part         and        quadric part
+        add    = np.matmul(SPREAD1, speed1) + np.matmul(SPREAD2, speed2)
+
+        control.power = add.tolist()        # - save sum of parts to message,
+        print(speed)
+        print(control.power)
         net.send(control)                    # - and send message to network.
