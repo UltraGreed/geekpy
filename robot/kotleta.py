b1771f0afb94d4f3145ba2588789f60419a1c8f8
helf4ch <kirill24579@gmail.com>
2023-07-17 15:33:05 +1000

    kotleta fix

--- robot/kotleta.py
+++ robot/kotleta.py
@@ -4,13 +4,10 @@ import setproctitle
 import dronecan
 from dronecan import uavcan
 
-sys.path.append('./')
 from base import message, network
 
 _PORT_NAME = 'can0'
-
-_MIN_POWER = 400
-_MAX_POWER = 4000
+_MAX_POWER = 7000
 
 
 def send_raw_command(node, power):
@@ -18,13 +15,7 @@ def send_raw_command(node, power):
     rotation_params = [ 1, 1, -1, 1, -1, 1 ]
 
     for i in range(len(cmd)):
-        current_power = round(power.power[i] / 100 * (_MAX_POWER - _MIN_POWER))
-
-        if current_power > 0:
-            current_power += _MIN_POWER
-        elif current_power < 0:
-            current_power -= _MIN_POWER
-
+        current_power = round(power.power[i] / 100 * _MAX_POWER)
         cmd[i] = current_power * rotation_params[i]
 
     message = uavcan.equipment.esc.RawCommand(cmd=cmd)
@@ -38,8 +29,6 @@ def main():
     power = message.Control()
 
     node = dronecan.make_node(_PORT_NAME, node_id=100, bitrate=500000)
-
-    node.add_handler(dronecan.uavcan.equipment.esc.Status, lambda msg: print(dronecan.to_yaml(msg)))
 
     while net.receive():
 
