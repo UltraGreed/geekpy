f94fd0bab8b5bad76194917b461a54fe2a2f0c1d
helf4ch <kirill24579@gmail.com>
2023-07-25 18:02:05 +1000

    photosave correct path

--- actions/photosave.py
+++ actions/photosave.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from datetime import datetime
 
 sys.path.append('./')
 from base import message, network
@@ -8,12 +9,15 @@ from base.timer import Timer
 
 def start(camera, path):
 	net = network.Net()
+
+	real_path = f'{path}_{camera}_{datetime.today().strftime("%Y%m%d_%H%M%S")}'
+
 	timer = Timer(0.5)
 	while True:
 		if timer.is_unlock:
 			break
 
-		net.send(message.PhotoOn(camera, path))
+		net.send(message.PhotoOn(camera, real_path))
 		time.sleep(0.01)
 
 
