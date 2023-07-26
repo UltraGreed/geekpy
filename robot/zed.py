92a05946aca6d1c58d23901b72cbadcbe4335e52
helf4ch <kirill24579@gmail.com>
2023-07-26 13:29:04 +1000

    fix savepath

--- robot/zed.py
+++ robot/zed.py
@@ -4,6 +4,7 @@ import os
 import socket
 import sys
 import time
+from datetime import datetime
 
 import numpy as np
 import pyzed.sl as sl
@@ -103,7 +104,7 @@ def main(name: str, serial: np.uint32, is_stream: bool = False, pose_tracking: b
         if net.id == "PhotoOn":
             if net.msg.camera == name:
                 img_capture = True
-                save_path = PATH_PREFIX + "/" + net.msg.folder
+                save_path = PATH_PREFIX + "/" + net.msg.folder + "_" +  name + "_" + datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
                 if not os.path.exists(save_path):
                     os.mkdir(save_path)
 
@@ -125,7 +126,8 @@ def main(name: str, serial: np.uint32, is_stream: bool = False, pose_tracking: b
             if photo_timer.is_unlock and img_capture:
                 photo_counter += 1
 
-                path = f"{save_path}/{name}_{photo_counter}.jpg"
+                file = f"{photo_counter:05d}.jpg"
+                path = f"{save_path}/{file}"
 
                 arr = image.get_data()
                 b, g, r, _ = Image.fromarray(arr).split()
@@ -139,7 +141,7 @@ def main(name: str, serial: np.uint32, is_stream: bool = False, pose_tracking: b
                 net.send(ImageLink(
                     obj=name,
                     path=path,
-                    file=f'{name}_{photo_counter}.jpg',
+                    file=file,
                     counter=photo_counter
                 ))
 
