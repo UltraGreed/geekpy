aad224db691f629821da4961fc242be724372d97
ultragreed <agent002004@gmail.com>
2023-08-04 18:39:32 +1000

    rov.sh physoptics disabled ctd enabled

--- rov.sh
+++ rov.sh
@@ -5,14 +5,14 @@
 
   cd robot
 
-  python physoptic.py &
+#  python physoptic.py &
   python navigation.py &
   python odometry.py &
   python regulator.py &
   python spreader.py &
   python kotleta.py &
 
-#  python ctd_i2c.py &
+  python ctd_i2c.py &
   python ctd_uart.py &
 
   python gpio.py &
