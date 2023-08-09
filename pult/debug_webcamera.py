06351a76621ddad09e7171559ba18b293bdca943
englred <konshilin@gmail.com>
2023-08-09 17:38:41 +1000

    Neural Network recognition + debug mode;

---
+++ pult/debug_webcamera.py
@@ -0,0 +1,108 @@
+from base import network, message
+from base.message import ImageLink
+import threading
+
+from PyQt6.QtMultimedia import QCamera, QMediaDevices, QImageCapture, QMediaCaptureSession
+from PyQt6.QtGui        import QPixmap
+from PyQt6.QtWidgets    import QApplication, QWidget, QHBoxLayout, QGraphicsScene, QGraphicsView
+from PyQt6.QtCore       import QTimer
+import sys
+
+
+class MainWindow(QWidget):
+    
+    def __init__(self, fps = 10):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle("PyQt6 Camera test")
+        self.setGeometry(0,0,720*2,750)
+        #Internal variables
+        self.counter = 0
+
+        #Gui
+        self.layout = QHBoxLayout(self)
+        
+        self.camera_device = QMediaDevices.defaultVideoInput()
+        self.camera        = QCamera(self.camera_device)
+
+        self.capture_session = QMediaCaptureSession()
+        self.capture_session.setCamera(self.camera)
+        
+        self.image_capture = QImageCapture(self.camera)
+        self.capture_session.setImageCapture(self.image_capture)
+
+        self.camera.start()
+        self.img_raw = QPixmap()
+
+        self.img_left = QGraphicsScene()
+        self.left_pixmap = self.img_left.addPixmap(self.img_raw)
+        self.img_left_view = QGraphicsView(self.img_left)
+        self.layout.addWidget(self.img_left_view)
+
+        self.img_right = QGraphicsScene()
+        self.right_pixmap = self.img_right.addPixmap(self.img_raw)
+        self.img_right_view = QGraphicsView(self.img_right)
+        self.layout.addWidget(self.img_right_view)
+
+        self.image_capture.imageCaptured.connect(self.image_captured)
+        self.image_capture.errorOccurred.connect(self.error)
+
+        self.net = network.Net()
+
+        self.fps_timer = QTimer()
+        self.fps_timer.setInterval(int(1.0/fps * 1000.0)) 
+        self.fps_timer.setSingleShot(False)
+        self.fps_timer.timeout.connect(self.capture_image)
+        self.fps_timer.start()
+
+        self.right_path = None
+        self.received = False
+
+        self.net_thread = threading.Thread(target = self.receive_picture)
+        self.net_thread.start()
+
+    def error(self, id, error, error_string):
+        print(error_string)
+
+    def capture_image(self):
+        self.image_capture.capture() 
+
+    def receive_picture(self):
+        while self.net.receive():
+            if self.net.id == "ImageLink":
+                print(self.net.msg.obj)
+                if self.net.msg.obj == "NN_CellB":
+                    self.right_path = self.net.msg.path     
+                    self.received = True       
+
+    def image_captured(self, id, image):   
+        pixmap = QPixmap.fromImage(image).copy(280,0,720,720)
+        self.left_pixmap.setPixmap(pixmap)
+
+        self.counter += 1
+        self.counter %= 20
+        pixmap.scaled(256,256).save(f"debug/{self.counter:05d}.png")
+        
+        self.net.send(ImageLink(
+            obj  = "Bottom",
+            path = f"debug/{self.counter:05d}.png",
+            file = f"{self.counter:05d}.png",
+            counter = self.counter
+        ))
+
+        while self.received == False:
+            pass
+
+        if self.right_path != None:
+            pixmap = QPixmap()
+            pixmap.load(self.right_path)
+            pixmap = pixmap.scaled(720,720)
+            self.right_pixmap.setPixmap(pixmap)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
+    
---
+++ robot/obj_recogn_nn.py
@@ -0,0 +1,191 @@
+import tensorflow as tf
+import numpy as np
+import time
+from datetime import datetime
+
+from tensorflow import keras
+from keras import Model, layers
+
+from base import network, message
+from base.message import ImageLink
+from object_recognition.model_class import InferenceModel
+
+import sys
+import os
+from pathlib import Path
+
+# ------------------------------------------ # 
+# Describe architecture of neural network    #
+# ------------------------------------------ #
+
+
+
+KERN_INIT   = 'he_uniform' #'glorot_uniform' #'ones' #'zeros'
+def Gen_Conv2D_Block(inp_layer, filters = 50, dilation = 1):
+    x = layers.Conv2D(filters = filters,
+                    kernel_size = 5,
+                    kernel_initializer = KERN_INIT,
+                    dilation_rate = dilation,
+                    padding = 'same')(inp_layer)
+    
+    x = layers.Activation("relu")(x)
+    x = layers.BatchNormalization()(x)
+    res = layers.Resizing(128,128, interpolation="bilinear")(x)
+    x = layers.MaxPooling2D(padding = 'same')(x)
+    return x, res
+
+def Gen_Conv2D_Block_11(inp_layer, filters = 50, act_func = "relu"):
+    x = layers.Conv2D(filters = filters,
+                    kernel_size = 1,
+                    kernel_initializer = KERN_INIT,
+                    padding = 'same')(inp_layer)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation(act_func)(x)
+    return x
+
+keras.saving.get_custom_objects().clear()
+
+@keras.saving.register_keras_serializable()
+class NeighborhoodDiff(keras.layers.Layer):
+    def __init__(self, area = 7):
+        super(NeighborhoodDiff, self).__init__()
+        self.area = area
+
+    def build(self, input_shape):
+        np_kernel = np.zeros([self.area, self.area, 3, 3])
+
+        x, y = np.meshgrid(np.arange(self.area), np.arange(self.area))
+        x -= self.area//2
+        y -= self.area//2
+        np_circle_mask = x**2 + y**2
+        np_circle_mask = (np_circle_mask <= (self.area//2)**2).astype('float64')
+        np_circle_mask /= np.count_nonzero(np_circle_mask)
+
+        np_kernel[::, ::, 0, 0] = np_circle_mask
+        np_kernel[::, ::, 1, 1] = np_circle_mask
+        np_kernel[::, ::, 2, 2] = np_circle_mask
+
+        self.kernel = tf.constant(np_kernel, dtype=tf.float32)
+
+    def get_config(self):
+        return {"area": self.area}
+
+    @tf.function
+    def calculate(self, inputs):
+        means = tf.raw_ops.Conv2D(input = inputs,
+                                    filter = self.kernel,
+                                    strides = [1,1,1,1],
+                                    padding = 'SAME')
+        return inputs - means
+
+    @tf.function
+    def call(self, inputs):
+        return tf.stop_gradient(self.calculate(inputs))
+
+def create_model():
+    INPUT_SHAPE = (128, 128, 3)
+
+    CONV_LAYERS = 8
+
+    Input         = layers.Input(INPUT_SHAPE)
+    Neigh_Layer1  = NeighborhoodDiff(13)(Input)
+    Input_Gen     = layers.Concatenate()([Input, Neigh_Layer1])
+    Stop_Grad     = layers.Lambda(lambda x: tf.stop_gradient(x))(Input_Gen)
+
+    Conv2D_Layers = []
+    Res_Layers    = []
+
+    # First Conv2D with connection to input
+
+    conv, res = Gen_Conv2D_Block(Stop_Grad)
+    Conv2D_Layers.append(conv)
+    Res_Layers.append(res)
+
+    for i in range(1, CONV_LAYERS):
+        conv, res = Gen_Conv2D_Block(Conv2D_Layers[len(Conv2D_Layers) - 1])
+        Conv2D_Layers.append(conv)
+        Res_Layers.append(res)
+
+    Merge_Layer     = layers.Concatenate()(Res_Layers)
+    Conv2D_11_Layer = Gen_Conv2D_Block_11(Merge_Layer, filters = 96)
+    Conv2D_11_Layer = Gen_Conv2D_Block_11(Conv2D_11_Layer, filters = 48)
+    Conv2D_11_Layer = Gen_Conv2D_Block_11(Conv2D_11_Layer, filters = 24)
+    Conv2D_11_Layer = Gen_Conv2D_Block_11(Conv2D_11_Layer, filters = 12)
+    Conv2D_11_Layer = Gen_Conv2D_Block_11(Conv2D_11_Layer, filters = 2, act_func = "softmax")
+
+    model = keras.Model(Input, Conv2D_11_Layer)
+
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                metrics=["accuracy"])
+    
+    return model
+
+# --------------------- #
+# Main code             #
+# --------------------- #
+
+#MAIN_OUT_FOLDER = os.environ['PYTHONPATH'] + "/debug/" #"/ssd/recognition_neural/"
+MAIN_OUT_FOLDER = "/ssd/recognition_neural/"
+NEURAL_MODELS   = "nn_models/"
+
+def Inference(model, image):
+    nn_img = tf.image.resize(image/255.0, [128, 128], "nearest").numpy()
+    nn_out = model.predict(nn_img[tf.newaxis, ...], verbose=0)
+    nn_out = tf.image.resize(nn_out, [256, 256], "nearest").numpy()[0, ..., 1]       
+    #print(nn_out.max(), nn_out.min())  
+    return nn_out   
+
+def main(argv):
+    datetime_now = datetime.now()
+    datetime_now_str = datetime_now.strftime("%d-%m-%Y-%H-%M-%S")
+    camera = argv[1]
+    object = argv[2]
+
+    save_folder = MAIN_OUT_FOLDER + f"/{camera}_{object}/{datetime_now_str}/"
+    try:
+        Path(save_folder).mkdir(parents=True, exist_ok=True)
+    except FileExistsError:
+        pass
+
+    net = network.Net()
+    model = create_model()
+    model.load_weights(f"nn_models/{object}/{object}").expect_partial()
+    print("Model loaded")
+    
+    counter = 0
+
+    inf_model = InferenceModel(lambda image: Inference(model, image))
+
+    while net.receive():
+        if net.id == "ImageLink":
+            if net.msg.obj == camera:
+                file    = tf.io.read_file(net.msg.path)
+                image   = tf.image.decode_png(file, channels = 3)
+
+                inf_model.check_object(image.numpy())
+
+                #net.send(message.DetectedObject(x=float(map_x), y=float(map_y), depth=float(map_depth), obj=obj_name))               
+
+                #nn_out = tf.stack([nn_out[0, ..., 1], nn_out[0, ..., 1],  nn_out[0, ..., 1]], axis = 2)      
+
+                #nn_out  = tf.cast(tf.argmax(nn_out, axis = -1), dtype=tf.float32)[0]
+                #nn_out  = tf.reshape(nn_out, [128, 128, 1])
+
+                file_name  = f"{counter:05}.png"
+
+                tf_img = tf.convert_to_tensor(inf_model.get_grayscale(), dtype=tf.uint8)
+                save_image = tf.image.encode_png(tf_img)
+                tf.io.write_file(save_folder + file_name, save_image)
+
+                net.send(ImageLink(
+                    path=save_folder + file_name,
+                    obj=f"NN_{object}",
+                    file=file_name,
+                    counter=counter
+                ))
+
+                counter += 1
+
+if __name__ == "__main__":
+    main(sys.argv)
