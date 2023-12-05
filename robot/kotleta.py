a38b71de34435e08a6f77c0dccbc953bfc3ad8d6
helf4ch <kirill24579@gmail.com>
2023-12-06 05:56:36 +1000

    second can in kotleta

--- robot/kotleta.py
+++ robot/kotleta.py
@@ -9,10 +9,10 @@ import numpy as np
 
 from base import message, network
 
-_PORT_NAME = 'can0'
-_MAX_POWER = 7000
+c0_to_c1 = { 2: 0, }
 
-_NO_SENSE = 200
+_MAX_POWER = 7000
+_NO_SENSE = 0.0285
 
 # Linear (1) & quadric (2) thrusters' spread by axis
 #######            X,      Y,   DEPTH,    YAW,  PITCH,   ROLL
@@ -24,9 +24,9 @@ SPREAD1 = [[ -300.00,   0.00, -250.00,   0.00,   1.00,   0.00],  # Thruster 0: b
            [  280.00,  67.00,    0.00,  -0.43,   0.00,   0.00]]  # Thruster 5: stern_right
 
 
-def send_raw_command(node, power):
+def send_raw_command(node_main, node_spare, power):
     cmd = [ 0, 0, 0, 0, 0, 0]
-    rotation_params = [ 1, 1, -1, 1, -1, 1 ]
+    rotation_params = [ 1, -1, -1, 1, -1, 1 ]
 
     for i in range(len(cmd)):
         sign = int(math.copysign(1, power.power[i]))
@@ -37,11 +37,15 @@ def send_raw_command(node, power):
 
         cmd[i] = current_power * rotation_params[i] * sign
 
-        if abs(cmd[i]) < _NO_SENSE:
+        if abs(cmd[i]) < _NO_SENSE * _MAX_POWER:
             cmd[i] = 0
 
-    message = uavcan.equipment.esc.RawCommand(cmd=cmd)
-    node.broadcast(message)
+    cmd_spare = [ 0, 0, 0, 0, 0, 0]
+    for key, value in c0_to_c1.items():
+        cmd_spare[value] = cmd[key] 
+
+    node_main.broadcast(uavcan.equipment.esc.RawCommand(cmd=cmd))
+    node_spare.broadcast(uavcan.equipment.esc.RawCommand(cmd=cmd_spare))
 
 
 def matrixing(speed):
@@ -54,20 +58,29 @@ def main():
     net = network.Net()
     power = message.Control()
 
-    node = dronecan.make_node(_PORT_NAME, node_id=100, bitrate=500000)
+    node_can0 = dronecan.make_node("can0", node_id=100, bitrate=500000)
+    node_can1 = dronecan.make_node("can1", node_id=101, bitrate=500000)
+
+    # node.add_handler(uavcan.equipment.esc.Status, lambda msg: print(dronecan.to_yaml(msg)))
+
+    # try:
+    #     node.spin()
+    # except KeyboardInterrupt:
+    #     return
 
     while net.receive():
 
         if net.id == "Control":
             power.power = net.msg.power
-            send_raw_command(node, power)
+            send_raw_command(node_can0, node_can1, power)
             continue
 
         if net.id == "Coord":
             power.power = matrixing(net.msg.speed)
-            send_raw_command(node, power)
+            send_raw_command(node_can0, node_can1, power)
 
-    node.close()
+    node_can0.close()
+    node_can1.close()
 
 
 if __name__ == '__main__':
