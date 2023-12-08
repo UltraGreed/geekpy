2b3874750e73df90873f665b8054bbab2b9c7510
ultragreed <agent002004@gmail.com>
2023-12-09 09:40:41 +1000

    test mission fix

--- tasks/test.py
+++ tasks/test.py
@@ -18,7 +18,7 @@ print(task_name, 'begin...')
 network.Net().send(InitRobot(yaw=0, depth=0))
 
 tack(origin='Navigation', yaw=0.0, dist=1.0, speed=0.0, depth=0.5)
-tack(origin='Navigation', yaw=-30, dist=1.0, speed=0.0, depth=0.5, dt=3)
+tack(origin='Navigation', yaw=30, dist=1.0, speed=0.0, depth=0.5, dt=3)
 tack(origin='Navigation', yaw=-30, dist=1.0, speed=0.0, depth=0.5, dt=3)
 tack(origin='Navigation', yaw=0.0, dist=1.0, speed=0.0, depth=0)
 
