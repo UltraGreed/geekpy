fa7ffddc72b9b15b5448844f1a14ccf3ddac3e5c
helf4ch <kirill24579@gmail.com>
2024-02-26 08:22:05 +1000

    get rid of additional can

--- robot/kotleta.py
+++ robot/kotleta.py
@@ -9,8 +9,6 @@ import numpy as np
 
 from base import message, network
 
-c0_to_c1 = { 2: 0, }
-
 _MAX_POWER = 7000
 _NO_SENSE = 0.0285
 
@@ -24,7 +22,7 @@ SPREAD1 = [[ -300.00,   0.00, -250.00,   0.00,   1.00,   0.00],  # Thruster 0: b
            [  280.00,  67.00,    0.00,  -0.43,   0.00,   0.00]]  # Thruster 5: stern_right
 
 
-def send_raw_command(node_main, node_spare, power):
+def send_raw_command(node, power):
     cmd = [ 0, 0, 0, 0, 0, 0]
     rotation_params = [ 1, -1, -1, 1, -1, 1 ]
 
@@ -40,12 +38,7 @@ def send_raw_command(node_main, node_spare, power):
         if abs(cmd[i]) < _NO_SENSE * _MAX_POWER:
             cmd[i] = 0
 
-    cmd_spare = [ 0, 0, 0, 0, 0, 0]
-    for key, value in c0_to_c1.items():
-        cmd_spare[value] = cmd[key] 
-
-    node_main.broadcast(uavcan.equipment.esc.RawCommand(cmd=cmd))
-    node_spare.broadcast(uavcan.equipment.esc.RawCommand(cmd=cmd_spare))
+    node.broadcast(uavcan.equipment.esc.RawCommand(cmd=cmd))
 
 
 def matrixing(speed):
@@ -58,29 +51,20 @@ def main():
     net = network.Net()
     power = message.Control()
 
-    node_can0 = dronecan.make_node("can0", node_id=100, bitrate=500000)
-    node_can1 = dronecan.make_node("can1", node_id=101, bitrate=500000)
-
-    # node.add_handler(uavcan.equipment.esc.Status, lambda msg: print(dronecan.to_yaml(msg)))
-
-    # try:
-    #     node.spin()
-    # except KeyboardInterrupt:
-    #     return
+    node = dronecan.make_node("can0", node_id=100, bitrate=500000)
 
     while net.receive():
 
         if net.id == "Control":
             power.power = net.msg.power
-            send_raw_command(node_can0, node_can1, power)
+            send_raw_command(node, power)
             continue
 
         if net.id == "Coord":
             power.power = matrixing(net.msg.speed)
-            send_raw_command(node_can0, node_can1, power)
+            send_raw_command(node, power)
 
-    node_can0.close()
-    node_can1.close()
+    node.close()
 
 
 if __name__ == '__main__':
