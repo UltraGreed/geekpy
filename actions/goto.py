4c8c5a14ab14a3ed18317956391e3eea57aa98fe
ultragreed <agent002004@gmail.com>
2024-03-03 18:18:40 +1000

    goto option to target coords

--- actions/goto.py
+++ actions/goto.py
@@ -1,49 +1,51 @@
 # @todo: Need to make lead_distance instead radius & wait time.
-import math, sys
 from base import mat, network, message
-from base.message import X, Y, YAW
 
 # Timer period to send 'Tack' message to regulator.
 TIMER = 0.25
 
-## Robot ahead moving function with given yaw
+
+# Robot ahead moving function with given yaw
 def goto(origin='', radius=1.0, speed=0.1, hold_time=0.0, depth=None):
-
     timer = 0.0
-    in_radius = False
 
-    # Initial and current robot positions and objects data.
-    pos   = network.wait_message('Coord').pos
-    obj   = network.wait_message('FilteredObjects').objs[origin]
+    # Initial and current robot position
+    auv_pos = network.wait_message('Coord').pos
 
-    # Infinit loop until reach destination.
+    # Either get object coords or use given coords
+    is_obj_oriented = origin is tuple
+    if is_obj_oriented:
+        target_pos = network.wait_message('FilteredObjects').objs[origin]
+    else:
+        target_pos = origin
+
+    # Infinite loop until auv reaches destination.
     net = network.Net(timer=TIMER)  # Wait for timer or message.
     while net.receive():            # Wait for timer ticks and messages.
-        # Send Tacks on timer and check conditions.
-        if net.id == 'Timer' and not in_radius:
-            yaw = mat.direction(pos, obj)                      # Calculate robot yaw to object position.
-            net.send(message.Tack(                             # Send Tack message
-                time=1.0,                                      # with this timeout
-                speed_x=0.0, speed_y=speed, stab_depth=depth,  # lateral and
-                stab_yaw=yaw, stab_pitch=0.0, stab_roll=0.0))  # angular values.
-        
-        if net.id == 'Timer' and mat.dist2d(pos, obj) <= radius:                  # If distance too short
-            in_radius = True
-            timer += TIMER
-            net.send(message.Tack(                             # Send Tack message
-                time=1.0,                                      # with this timeout
-                speed_x=0.0, speed_y=0.0, stab_depth=depth,  # lateral and
-                stab_yaw=None, stab_pitch=0.0, stab_roll=0.0))  # angular values.
+        # if auv is far from target
+        if net.id == 'Timer' and mat.dist2d(auv_pos, target_pos) > radius:
+            yaw = mat.direction(auv_pos, target_pos)  # Calculate relative yaw to object position.
+            net.send(message.Tack(
+                time=1.0,
+                speed_x=0.0, speed_y=speed,
+                stab_depth=depth, stab_yaw=yaw, stab_pitch=0.0, stab_roll=0.0
+            ))
+
+        # if auv is close to target
+        if net.id == 'Timer' and mat.dist2d(auv_pos, target_pos) <= radius:
+            timer += TIMER   # time when auv is close to target
+            net.send(message.Tack(
+                time=1.0,
+                speed_x=0.0, speed_y=0.0,
+                stab_depth=depth, stab_yaw=None, stab_pitch=0.0, stab_roll=0.0
+            ))
             if timer >= hold_time:
                 return
 
-        if net.id == 'Timer' and  mat.dist2d(pos, obj) > radius:
-            in_radius = False
-
         # Save robot position.
         elif net.id == 'Coord':
-            pos = net.msg.pos
+            auv_pos = net.msg.pos
 
-        # Save objects position.
-        elif net.id == 'FilteredObjects':
-            obj = net.msg.objs[origin]
+        # Change object position.
+        elif net.id == 'FilteredObjects' and is_obj_oriented:
+            target_pos = net.msg.objs[origin]
