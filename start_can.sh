85ef5aee4adbe9a3b47fa19ddafd43abc37b52e2
helf4ch <kirill24579@gmail.com>
2024-04-16 18:31:27 +1000

    fix: ctd maybe works

--- start_can.sh
+++ start_can.sh
@@ -15,3 +15,5 @@ echo out > /sys/class/gpio/gpio392/direction
 echo out > /sys/class/gpio/gpio394/direction
 echo out > /sys/class/gpio/gpio395/direction
 echo out > /sys/class/gpio/gpio396/direction
+
+sh -c "echo 800000 > /sys/bus/i2c/devices/i2c-0/bus_clk_rate"
