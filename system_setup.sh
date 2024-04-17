d646e931b8f54fe29106e5553a3053a28b6bf6a4
ultragreed <agent002004@gmail.com>
2024-04-17 18:12:02 +1000

    feat(scripts): setup scripts joint in one script

---
+++ system_setup.sh
@@ -0,0 +1,21 @@
+sudo timedatectl set-time $1
+
+modprobe can
+modprobe can-raw
+modprobe mttcan
+ip link set can0 up type can bitrate 500000
+ip link set can1 up type can bitrate 500000
+
+echo 392 > /sys/class/gpio/export
+echo 394 > /sys/class/gpio/export
+echo 395 > /sys/class/gpio/export
+echo 396 > /sys/class/gpio/export
+
+sleep 0.2
+
+echo out > /sys/class/gpio/gpio392/direction
+echo out > /sys/class/gpio/gpio394/direction
+echo out > /sys/class/gpio/gpio395/direction
+echo out > /sys/class/gpio/gpio396/directionmodprobe can
+
+sh -c "echo 800000 > /sys/bus/i2c/devices/i2c-0/bus_clk_rate"
