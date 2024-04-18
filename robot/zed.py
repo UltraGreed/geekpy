5487b6751d55a7b9940672b26e19338bb126bc9c
hedgehog319 <fign1910@mail.ru>
2024-04-18 11:46:48 +1000

    feat: Disable the depth map if not use

--- robot/zed.py
+++ robot/zed.py
@@ -354,7 +354,9 @@ def main(
     init_params.coordinate_units = sl.UNIT.METER
     # init_params.depth_minimum_distance = 0  # Units equals coordinate_units
     init_params.depth_maximum_distance = 5  # Units equals coordinate_units
-    init_params.depth_mode = sl.DEPTH_MODE.ULTRA
+    init_params.depth_mode = (
+        sl.DEPTH_MODE.ULTRA if save_mode & SaveMode.Depth else sl.DEPTH_MODE.NONE
+    )
     init_params.camera_fps = 30
     init_params.camera_image_flip = sl.FLIP_MODE.OFF
 
