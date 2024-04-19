2325dea92ca0ce80a6b97ea08b9fb402247bf9bb
ultragreed <agent002004@gmail.com>
2024-04-20 04:07:29 +1000

    some small adjustments for current state of work

--- object_recognition/object_position.py
+++ object_recognition/object_position.py
@@ -11,6 +11,7 @@ def get_rel_from_pixel(image_shape, obj_coords, camera_dist):
     pixel_relative_x = image_shape[X] / 2 - obj_coords[X]
     pixel_relative_y = obj_coords[Y] - image_shape[Y] / 2
 
+    # TODO: Split camera FOVs
     relative_x = pixel_relative_y / image_shape[Y] * 2 * math.tan(CAMERA_FOV / 2 / math.pi * 180) * camera_dist
     relative_y = pixel_relative_x / image_shape[X] * 2 * math.tan(CAMERA_FOV / 2 / math.pi * 180) * camera_dist
 
@@ -30,6 +31,7 @@ def get_obj_pos_bottom(robot_pos, obj_depth, image_shape, obj_center):
 
 
 def get_obj_pos_front(robot_pos, obj_real, obj_pixel, image_shape, obj_center):
+    # TODO: Split camera FOVs
     camera_dist_x = image_shape[X] / obj_pixel[X] / math.tan(CAMERA_FOV / 2 / math.pi * 180) * obj_real / 2
     camera_dist_y = image_shape[Y] / obj_pixel[Y] / math.tan(CAMERA_FOV / 2 / math.pi * 180) * obj_real / 2
 
@@ -38,6 +40,7 @@ def get_obj_pos_front(robot_pos, obj_real, obj_pixel, image_shape, obj_center):
     relative_x, relative_depth = get_rel_from_pixel(image_shape, obj_center, camera_dist)
     relative_depth *= -1
 
+    # TODO: Split camera FOVs
     relative_y = camera_dist * math.cos(CAMERA_FOV / 2 / math.pi * 180)
 
     map_x, map_y, map_depth = mat.robot2map(robot_pos, (relative_x, relative_y, relative_depth))
--- tasks/test.py
+++ tasks/test.py
@@ -17,9 +17,9 @@ print(task_name, 'begin...')
 
 network.Net().send(InitRobot(yaw=0, depth=0))
 
-tack(mode='Absolute', yaw=0.0, dist=1.0, speed=0.1, depth=0.5, dt=1)
-tack(mode='Absolute', yaw=30, dist=1.0, speed=0.0, depth=0.5, dt=2)
-tack(mode='Absolute', yaw=-30, dist=1.0, speed=0.0, depth=0.5, dt=2)
-tack(mode='Absolute', yaw=0.0, dist=1.0, speed=-0.2, depth=0)
+tack(mode='Absolute', yaw=0.0, dist=0, speed=0, depth=0.5, dt=10)
+# tack(mode='Absolute', yaw=0.0, dist=2.0, speed=0.3, depth=0.5)
+# tack(mode='Absolute', yaw=180, dist=0.0, speed=0.0, depth=0.5, dt=3)
+# tack(mode='Absolute', yaw=180, dist=2.0, speed=0.3, depth=0.5)
 
 print(' '.join(sys.argv), "end!")
