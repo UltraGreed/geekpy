92997e82208d0d1dc0c8f75b0bfa8936cd2bc659
ultragreed <agent002004@gmail.com>
2024-07-04 14:20:55 +1000

    fix: .venv added to ignore

--- install_all.sh
+++ install_all.sh
@@ -1,2 +1,2 @@
 # Install all sofware to robot
-rsync -vIpzrt --exclude .git --exclude venv --exclude __pycache__ --exclude "*.png" --exclude "*.jpg"  . auv@192.168.88.101:geekpy
+rsync -vIpzrt --exclude .git --exclude venv --exclude .venv --exclude __pycache__ --exclude "*.png" --exclude "*.jpg"  . auv@192.168.88.101:geekpy
