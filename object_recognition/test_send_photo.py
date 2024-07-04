27691a204862caae0e389b819f546ae730b52224
ultragreed <agent002004@gmail.com>
2024-07-04 14:33:44 +1000

    fix: imagelink sender fix

--- object_recognition/test_send_photo.py
+++ object_recognition/test_send_photo.py
@@ -1,11 +1,11 @@
-from os import walk
+from os import walk, path
 from time import sleep
 import sys
 
 from base import network
 from base import message
 
-FOLDER_PATH = sys.argv[1]
+FOLDER_PATH = path.abspath(sys.argv[1])
 OBJECT = tuple(sys.argv[2].split("."))
 
 
@@ -15,9 +15,8 @@ for (dirpath, dirnames, filenames) in walk(FOLDER_PATH):
     break
 
 net = network.Net()
-msg = message.ImageLink(obj=OBJECT[0], path={OBJECT[1]: ""})
 
 for it in files:
-    msg.path[OBJECT[1]] = FOLDER_PATH + it
+    msg = message.ImageLinkCameraStereo(obj=OBJECT[0], path_right=f"{FOLDER_PATH}/{it}")
     net.send(msg)
-    sleep(0.5)
+    sleep(0.1)
