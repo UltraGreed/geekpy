27147383b26e2bba090cf4437e12e9aa74e7f3b0
ultragreed <agent002004@gmail.com>
2024-11-28 03:06:43 +1000

    feat(recognition): use uint8 instead of float64
    Only line was tested so far

--- object_recognition/histograms.py
+++ object_recognition/histograms.py
@@ -7,35 +7,49 @@ from config import *
 
 
 obj_name = sys.argv[1]
-COLOR_AMOUNT = tuple(RGB_AMOUNT for _ in range(3)) if COLOR_SCHEME == 'RGB' else (H_AMOUNT, S_AMOUNT, V_AMOUNT)
 
 
 def get_histogram_separate(axel_ind):
-    image_g = np.zeros(COLOR_AMOUNT[:axel_ind] + COLOR_AMOUNT[axel_ind + 1:], dtype='uint8')
+    image_g = np.zeros(MODEL_SHAPE[:axel_ind] + MODEL_SHAPE[axel_ind + 1:], dtype='uint8')
 
-    image_r = np.round(np.max(data_obj_norm, axis=axel_ind) * 255)
-    image_b = np.round(np.max(data_non_obj_norm, axis=axel_ind) * 255)
+    image_r = np.max(data_obj_norm, axis=axel_ind) // MODEL_IMAGE_CAST
+    image_b = np.max(data_non_obj_norm, axis=axel_ind) // MODEL_IMAGE_CAST
 
-    return np.stack((image_r.astype('uint8'), image_g, image_b.astype('uint8')), axis=-1)
+    return np.stack(
+        (image_r.astype(np.uint8),
+         image_g.astype(np.uint8),
+         image_b.astype(np.uint8)),
+        axis=-1,
+    )
 
 
 def get_histogram_model(axel_ind):
-    image_g = np.zeros(COLOR_AMOUNT[:axel_ind] + COLOR_AMOUNT[axel_ind + 1:], dtype='uint8')
+    image_g = np.zeros(MODEL_SHAPE[:axel_ind] + MODEL_SHAPE[axel_ind + 1:], dtype='uint8')
 
-    image_r = np.round(np.max(data_sub, axis=axel_ind) * 255)
-    image_b = np.where(np.max(data_sub, axis=axel_ind) == 0, 255, 0)
+    image_r = np.max(data_sub, axis=axel_ind) // MODEL_IMAGE_CAST
 
-    return np.stack((image_r.astype('uint8'), image_g, image_b.astype('uint8')), axis=-1)
+    image_b = np.where(
+        np.max(data_sub, axis=axel_ind) == 0,
+        np.asarray(255, dtype=np.uint8),
+        np.asarray(0, dtype=np.uint8),
+    )
+
+    return np.stack(
+        (image_r.astype(np.uint8),
+         image_g.astype(np.uint8),
+         image_b.astype(np.uint8)),
+        axis=-1,
+    )
 
 
-data_obj_norm = np.load(get_model_path(obj_name, 'obj_norm')).reshape(COLOR_AMOUNT)
-data_non_obj_norm = np.load(get_model_path(obj_name, 'noobj_norm')).reshape(COLOR_AMOUNT)
-data_sub = np.load(get_model_path(obj_name, 'sub')).reshape(COLOR_AMOUNT)
+data_obj_norm = np.load(get_model_path(obj_name, 'obj_norm')).reshape(MODEL_SHAPE)
+data_non_obj_norm = np.load(get_model_path(obj_name, 'noobj_norm')).reshape(MODEL_SHAPE)
+data_sub = np.load(get_model_path(obj_name, 'sub')).reshape(MODEL_SHAPE)
 
-save_image_rgb('hist_sep_23.png', get_histogram_separate(0))
-save_image_rgb('hist_sep_13.png', get_histogram_separate(1))
-save_image_rgb('hist_sep_12.png', get_histogram_separate(2))
+save_image_rgb('hist_sep_SV.png', get_histogram_separate(0))
+save_image_rgb('hist_sep_HV.png', get_histogram_separate(1))
+save_image_rgb('hist_sep_HS.png', get_histogram_separate(2))
 
-save_image_rgb('hist_model_23.png', get_histogram_model(0))
-save_image_rgb('hist_model_13.png', get_histogram_model(1))
-save_image_rgb('hist_model_12.png', get_histogram_model(2))
+save_image_rgb('hist_model_SV.png', get_histogram_model(0))
+save_image_rgb('hist_model_HV.png', get_histogram_model(1))
+save_image_rgb('hist_model_HS.png', get_histogram_model(2))
--- object_recognition/normalize.py
+++ object_recognition/normalize.py
@@ -19,32 +19,31 @@ data_all = np.load(get_model_path(obj_name, model_id='all'))
 object_max = np.max(data_object)
 non_object_max = np.max(data_non_object)
 
-data_object_norm = data_object / object_max
-data_non_object_norm = data_non_object / non_object_max
+data_object_norm = (data_object * (MODEL_MAX_VALUE / object_max)).astype(MODEL_DTYPE)
+data_non_object_norm = (data_non_object * (MODEL_MAX_VALUE / non_object_max)).astype(MODEL_DTYPE)
 
 # Cut off the upper borders
-data_object_norm = np.where(
-    data_object_norm <= UPPER_BORDER_OBJECT,
-    data_object_norm / UPPER_BORDER_OBJECT,
-    1
-)
-data_non_object_norm = np.where(
-    data_non_object_norm <= UPPER_BORDER_NON_OBJECT,
-    data_non_object_norm / UPPER_BORDER_NON_OBJECT,
-    1
-)
+data_object_norm[
+    data_object_norm > UPPER_BORDER_OBJECT * MODEL_MAX_VALUE
+] = UPPER_BORDER_OBJECT * MODEL_MAX_VALUE
+data_object_norm = (data_object_norm / UPPER_BORDER_OBJECT).astype(MODEL_DTYPE)
+
+data_non_object_norm[
+    data_non_object_norm > UPPER_BORDER_OBJECT * MODEL_MAX_VALUE
+] = UPPER_BORDER_OBJECT * MODEL_MAX_VALUE
+data_non_object_norm = (data_non_object_norm / UPPER_BORDER_OBJECT).astype(MODEL_DTYPE)
 
 np.save(get_model_path(obj_name, model_id='obj_norm'), data_object_norm)
 np.save(get_model_path(obj_name, model_id='noobj_norm'), data_non_object_norm)
 
-data_sub = data_object_norm - data_non_object_norm
+data_sub = data_object_norm.astype(np.int64) - data_non_object_norm
 
 # Normalize the subtraction model
-data_sub_norm = np.where(
-    data_sub >= LOWER_MODEL_BORDER,
-    (data_sub - LOWER_MODEL_BORDER) / (1 - LOWER_MODEL_BORDER),
-    0
-)
+data_sub_norm = (np.where(
+    data_sub > LOWER_MODEL_BORDER * MODEL_MAX_VALUE,
+    data_sub - LOWER_MODEL_BORDER * MODEL_MAX_VALUE,
+    0,
+) / (1 - LOWER_MODEL_BORDER)).astype(MODEL_DTYPE)
 
 np.save(get_model_path(obj_name, model_id='sub'), data_sub_norm)
 
