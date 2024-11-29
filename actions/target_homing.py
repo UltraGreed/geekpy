0a5d020c6daabc45839cc3c5a8178042caaf6e26
ultragreed <agent002004@gmail.com>
2024-11-29 17:34:08 +1000

    fix: target homing

--- actions/target_homing.py
+++ actions/target_homing.py
@@ -1,5 +1,5 @@
 from base import network, message
-from actions.tack import tack
+from base.message import YAW
 
 
 def target_homing(depth: float, speed: float):
@@ -10,14 +10,19 @@ def target_homing(depth: float, speed: float):
     :param speed: speed of Tack message
     :return:
     """
+    pos = network.wait_message('Coord').pos
     net = network.Net(timer=0.25)
     while net.receive():
-        if net.id == message.Target and net.msg.is_detected:
-            tack(
-                priority=1,
-                mode='Relative',
-                depth=depth,
-                dt=1,
-                speed=speed,
-                yaw=net.msg.offset_yaw,
-            )
+        if net.id == message.Target.id and net.msg.is_detected:
+            net.send(message.Tack(
+                time=0.5,
+                speed_y=speed,
+                speed_x=0,
+                stab_depth=depth,
+                stab_yaw=net.msg.offset_yaw + pos[YAW],
+                stab_pitch=0.0,
+                stab_roll=0.0
+            ))
+
+        if net.id == message.Coord.id:
+            pos = net.msg.pos
